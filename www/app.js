cdcf16c0bd4c67ccaddeb8d1029621638a9d270c
Luka Zitnik <luka.zitnik@gmail.com>
2014-10-18 17:02:03 +0200

    add new expense to view, no db insert yet

--- www/app.js
+++ www/app.js
@@ -20,10 +20,83 @@ function DailyExpenses(month, dailyExpenses) {
     });
 
     this.month = month;
+
+    this.updateDailyExpensesList = function (date, amount) {
+        var dailySum = this.findDailySum(date);
+
+        if ((dailySum instanceof DailySum) === false) {
+            dailySum = new DailySum(date, amount);
+            this.dailyExpensesList.push(dailySum);
+        }
+        else {
+            dailySum.dailySum(dailySum.dailySum() + amount);
+        }
+    };
+
+    this.findDailySum = function (date) {
+        var list = this.dailyExpensesList(),
+            i = 0;
+
+        for (; i < list.length; ++i) {
+            if (list[i].date === date) {
+                return list[i];
+            }
+        }
+
+        return null;
+    };
 }
 
 function MonthlyExpenses(monthlyExpenses) {
     this.monthlyExpensesList = ko.observableArray(monthlyExpenses);
+
+    this.addExpense = function () {
+        var formElements = document.getElementById("add-expense-view").querySelector("form").elements,
+            label = formElements.label.value,
+            date = formElements.date.value,
+            month = date.substring(0, 7),
+            amount = parseFloat(formElements.amount.value);
+
+        this.closeAddExpenseView();
+        this.updateMonthlyExpensesList(month, date, amount);
+        this.persistIntoDB(date, label, amount);
+    };
+
+    this.updateMonthlyExpensesList = function (month, date, amount) {
+        var dailyExpenses = this.findDailyExpenses(month);
+
+        if ((dailyExpenses instanceof DailyExpenses) === false) {
+            dailyExpenses = new DailyExpenses(month, []);
+            this.monthlyExpensesList.push(dailyExpenses);
+        }
+
+        dailyExpenses.updateDailyExpensesList(date, amount);
+    };
+
+    this.findDailyExpenses = function (month) {
+        var list = this.monthlyExpensesList(),
+            i = 0;
+
+        for (; i < list.length; ++i) {
+            if (list[i].month === month) {
+                return list[i];
+            }
+        }
+
+        return null;
+    };
+
+    this.persistIntoDB = function (date, label, amount) {
+
+    };
+
+    this.openAddExpenseView = function () {
+        document.getElementById("add-expense-view").setAttribute("aria-hidden", "false");
+    };
+
+    this.closeAddExpenseView = function () {
+        document.getElementById("add-expense-view").setAttribute("aria-hidden", "true");
+    };
 }
 
 function writeTestDBData() {
