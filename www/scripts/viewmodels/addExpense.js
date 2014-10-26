9c0ac5ac8354b7b6fa0573d7fa0e7d461ec83b6f
Luka Zitnik <luka.zitnik@gmail.com>
2014-10-26 17:46:48 +0100

    refactor addExpense view model

--- www/scripts/viewmodels/addExpense.js
+++ www/scripts/viewmodels/addExpense.js
@@ -1,26 +1,33 @@
 var addExpense = {
 
     containerNode: document.getElementById("add-expense-view"),
+    label: ko.observable(),
+    amount: ko.observable(),
+    date: ko.observable(),
 
     open: function() {
         this.containerNode.setAttribute("aria-hidden", "false");
     },
 
+    formIsValid: function() {
+        return this.containerNode.querySelector("form").checkValidity();
+    },
+
     addExpense: function () {
-        var form = this.containerNode.querySelector("form"),
-            formElements = form.elements,
-            label = formElements.label.value,
-            date = formElements.date.value,
-            month = date.substring(0, 7),
-            amount = parseFloat(formElements.amount.value);
+        var month = this.date().substring(0, 7),
+            amount = parseFloat(this.amount());
 
-        if (form.checkValidity() === false) {
-            return;
-        }
-
-        monthlyExpenses.updateMonthlyExpensesList(month, date, amount);
+        monthlyExpenses.updateMonthlyExpensesList(
+            month,
+            this.date(),
+            amount
+        );
         this.close();
-        this.persistIntoDB(date, label, amount);
+        this.persistIntoDB(
+            this.date(),
+            this.label(),
+            amount
+        );
     },
 
     persistIntoDB: function (date, label, amount) {
