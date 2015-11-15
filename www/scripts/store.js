bf3fae7e9e6f1bdba9d0a816ce80a17e7f3b3642
Luka Zitnik <luka.zitnik@gmail.com>
2015-11-15 23:16:22 +0100

    transform old db records

--- www/scripts/store.js
+++ www/scripts/store.js
@@ -1,8 +1,8 @@
 var store = {
 
-    dbName: "Expenditures",
+    dbName: "Personal Budget",
     dbVersion: 1,
-    storeName: "expenditures",
+    storeName: "records",
 
     checkSupport: function() {
         if (!window.indexedDB) {
@@ -99,7 +99,7 @@ var store = {
         };
     },
 
-    add: function (storeValue) {
+    add: function (storeValues, doneCallback) {
         var self = this,
             openDBRequest;
 
@@ -114,9 +114,21 @@ var store = {
         openDBRequest.onsuccess = function (event) {
             var db = event.target.result,
                 transaction = db.transaction(self.storeName, "readwrite"),
-                objectStore = transaction.objectStore(self.storeName);
+                objectStore = transaction.objectStore(self.storeName),
+                i = 0,
+                limit;
 
-            objectStore.add(storeValue);
+            if (storeValues instanceof Array === false) {
+                storeValues = [storeValues];
+            }
+
+            limit = storeValues.length;
+
+            for (; i < limit; ++i) {
+                objectStore.add(storeValues[i]);
+            }
+
+            doneCallback();
         };
     },
 
@@ -125,11 +137,50 @@ var store = {
     },
 
     dbUpgradeNeededHandler: function (event) {
-        var db = event.target.result,
+        var self = this,
+            db = event.target.result,
             objectStore;
 
-        objectStore = db.createObjectStore(this.storeName, {autoIncrement: true});
+        function exportOldExpendituresStore (doneCallback) {
+            var openDBRequest = window.indexedDB.open("Expenditures", 1);
+
+            openDBRequest.onsuccess = function (event) {
+                var db = event.target.result,
+                    transaction,
+                    objectStore,
+                    aggregatedStoreValues;
+
+                if (db.objectStoreNames.length === 0) {
+                    return;
+                }
+
+                transaction = db.transaction("expenditures", "readonly");
+                objectStore = transaction.objectStore("expenditures");
+                aggregatedStoreValues = [];
+
+                objectStore.openCursor().onsuccess = function (event) {
+                    var cursor = event.target.result;
+
+                    if (cursor) {
+                        cursor.value.value *= -1;
+                        aggregatedStoreValues.push(cursor.value);
+                        cursor.continue();
+                    }
+                    else {
+                        doneCallback(aggregatedStoreValues);
+                    }
+                };
+            };
+        }
+
+        objectStore = db.createObjectStore(self.storeName, {autoIncrement: true});
         objectStore.createIndex("date", "date", {unique: false});
+        exportOldExpendituresStore(function (storeValues) {
+            self.add(storeValues, function () {
+                window.indexedDB.deleteDatabase("Expenditures");
+                window.location.reload();  // Cheap way to compensate for missing synchronization
+            });
+        });
     },
 
 	clear: function () {
